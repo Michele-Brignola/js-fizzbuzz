0c268b5879c5fcf3cca7e87b3bad7a21a35452de
Michele-Brignola <michele.brignola@hotmail.com>
2025-12-17 15:26:56 +0100

    added bonus

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
+// * Soluzione 1:
+// // * For Loop fino da 1 a 100
+// for (let i = 1; i <= 100; i++) {
+//   // * Se multiplo di 15: Output "FizzBuzz"
+//   if (i % 15 === 0) { console.log("FizzBuzz"); }
+//   // * Altrimenti Se multiplo di 5: Output "Buzz"
+//   else if (i % 5 === 0) { console.log("Buzz"); }
+//   // * Altrimenti Se multiplo di 3: Output "Fizz"
+//   else if (i % 3 === 0) { console.log("Fizz"); }
+//   // * Altrimenti: Output i
+//   else { console.log(i); }
+
+
+// * Soluzione 2:
+let output = "";
+
 // * For Loop fino da 1 a 100
 for (let i = 1; i <= 100; i++) {
-  // * Se multiplo di 15: Output "FizzBuzz"
-  if (i % 15 === 0) { console.log("FizzBuzz"); }
-  // * Altrimenti Se multiplo di 5: Output "Buzz"
-  else if (i % 5 === 0) { console.log("Buzz"); }
-  // * Altrimenti Se multiplo di 3: Output "Fizz"
-  else if (i % 3 === 0) { console.log("Fizz"); }
-  // * Altrimenti: Output i
-  else { console.log(i); }
+  output = "";
+
+  // * Se multiplo di 3: Output += "Fizz"
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+
+  // * Se multiplo di 5: Output += "Buzz"
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+
+  if (output !== "") {
+    console.log(output);
+  } else {
+    console.log(i);
+  }
 }
