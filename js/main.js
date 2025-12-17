1d2b5ec298995291674ea7d87c24bab60fbce70f
Michele-Brignola <michele.brignola@hotmail.com>
2025-12-17 12:51:31 +0100

    added comments

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-for (let i = 0; i <= 100; i += 1) {
-  if ((i % 3 === 0) && (i % 5 === 0)) {
-    console.log("FizzBuzz");
-  }
-  else if (i % 3 === 0) {
-    console.log("Fizz");
-  }
-  else if (i % 5 === 0) {
-    console.log("Buzz");
-  }
-  else {
-    console.log(i);
-  }
+// * For Loop fino da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  // * Se multiplo di 15: Output "FizzBuzz"
+  if (i % 15 === 0) { console.log("FizzBuzz"); }
+  // * Altrimenti Se multiplo di 5: Output "Buzz"
+  else if (i % 5 === 0) { console.log("Buzz"); }
+  // * Altrimenti Se multiplo di 3: Output "Fizz"
+  else if (i % 3 === 0) { console.log("Fizz"); }
+  // * Altrimenti: Output i
+  else { console.log(i); }
 }
